19a22b0b3e4c5e109e82dd56b55be61b6a894cba
TSytpp <Tolgasoytepe@gmail.com>
2022-07-23 23:18:06 +0300

    Arduino İle led Kontrolü

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <Arduino.h>
-#include<RF24.h>
 
+
+//DONANIM PLUS ELEKTRONİK
+//TASARIM TOLGA SOYTEPE
+//İNSTAGRAM Tolgasyp
+//TWİTTER TSytpp
+//YOUTUBE KANALI https://www.youtube.com/channel/UCinFB4XVJ3Yb6QDnALUlccw
+int led = 3; //ledimizi 3.pine bağladık..
 
 void setup() {
- pinMode(3,OUTPUT);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+pinMode(led,OUTPUT); // led pinimizi çıkış olarak tanımladık..
+
+}
+void loop(){
+digitalWrite(led,HIGH); // led pinimize güç verdik
+delay(1000); //ardından bir saniye bekledik..
+digitalWrite(led,LOW); // led pinimizin gücünü kestik..
+delay(1000); // ve ardından tekrar bir saniye bekledik ve başa döndük böylece ledimiz 1 er saniyelik aralıklarla yanıp sönmüş oldu.. :)
 }
